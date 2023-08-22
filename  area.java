414c396aeff16b7b97d10e82bc48c3ff6cc7efda
Rithicksam55 <142867715+Rithicksam55@users.noreply.github.com>
2023-08-22 05:38:14 +0000

    complete the java cricle

---
+++  area.java
@@ -0,0 +1,7 @@
+class area{
+    public static void main(String args[]){
+        double r=10,pi=3.141519;
+        double a=pi*r*r;
+        System.out.println("the area of the cricle is:"+a);
+    }        
+}
